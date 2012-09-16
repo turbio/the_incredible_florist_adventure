b280f7c7e175b04932c88bc44ccfd64b0965b1f3
captain777 <masonclayton@live.com>
2012-09-16 06:18:51 -0700

    added texture loading system

--- tifa/tifa/LogicHandler.cpp
+++ tifa/tifa/LogicHandler.cpp
@@ -4,9 +4,24 @@
 #define HEIGHT 480
 #define TITLE "The Incredible Florist Adventure"
 
+
+sf::Sprite sprite;
+sf::RectangleShape shape;
+
 //CONSTRUCTOR
 LogicHandler::LogicHandler(void){
-	window = new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), TITLE);
+	window = new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), TITLE, sf::Style::Close);
+
+	shape.setSize(sf::Vector2f(10.0f, 10.0f));
+	if(!LogicHandler::loadTextures("res/")){
+		shape.setFillColor(sf::Color::Red);
+	}else{
+		shape.setFillColor(sf::Color::Green);
+	}
+
+	sprite.setTexture(*textureList.at(1));
+	//sprite.setTextureRect(sf::IntRect(10, 10, 50, 30));
+	sprite.setPosition(0, 0);
 }
 
 //DESTRUCTOR
@@ -15,21 +30,62 @@ LogicHandler::~LogicHandler(void){
 }
 
 void LogicHandler::run(void){
+
+	double deltaTime = 0;
+	sf::Clock clock;
 	while (window->isOpen()){
 		sf::Event event;
 		while (window->pollEvent(event)){
 			handleEvent(event);
         }
+		
+		clock.restart();
+		LogicHandler::update(deltaTime);
+
 		window->clear(sf::Color(255, 0, 255));
+
+		//render
+		window->draw(sprite);
+		window->draw(shape);
+
+		deltaTime = clock.getElapsedTime().asMilliseconds();
+
 		window->display();
     }
 }
 
-void LogicHandler::update(void){
+void LogicHandler::update(double delta){
 
 }
 
 void LogicHandler::handleEvent(sf::Event evt){
 	if (evt.type == sf::Event::Closed)
-				window->close();
+		window->close();
+}
+
+bool LogicHandler::loadTextures(std::string dir){
+	std::string line;
+	std::vector<sf::Texture*> textures;
+
+	std::ifstream file(dir + "res.txt");
+
+	if(file.is_open()){
+		while(file.good()){
+			std::getline(file, line);
+
+			sf::Texture *tex;
+			tex = new sf::Texture;
+			if(!tex->loadFromFile( dir + line)){
+				return false;
+			}
+			textures.push_back(tex);
+			
+		}
+		file.close();
+	}else{
+		return false;
+	}
+
+	textureList = textures;
+	return true;
 }
--- tifa/tifa/LogicHandler.h
+++ tifa/tifa/LogicHandler.h
@@ -1,15 +1,20 @@
 #pragma once
 #include <SFML\Graphics.hpp>	//uses graphics cuz itz a game dur
+#include <fstream>
+#include <string>
+#include <vector>
 
 class LogicHandler{
 
 public:
 	LogicHandler(void);	//CONSTRUCTOR
 	~LogicHandler(void);	//DESTRUCTOR
-	void update(void);	//update game stuff
+	void update(double delta_time);	//update game stuff
 	void run(void);	//main game loop
 
 private:
 	void handleEvent(sf::Event theEventToBeHandled);	//handles any and all events
+	bool loadTextures(std::string directory);
 	sf::RenderWindow *window;	//the main window (probobaly the only)
+	std::vector<sf::Texture*> textureList;
 };
