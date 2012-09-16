b19bcf043394e3127d531dcf1c6e2d352810f954
captain777 <masonclayton@live.com>
2012-09-16 07:12:08 -0700

    added game start and coulds woooooo

--- tifa/tifa/LogicHandler.cpp
+++ tifa/tifa/LogicHandler.cpp
@@ -5,11 +5,13 @@
 #define TITLE "The Incredible Florist Adventure"
 
 
-sf::Sprite sprite;
+sf::Sprite titleScreenBg, cloud1, cloud2;
 sf::RectangleShape shape;
 
 //CONSTRUCTOR
 LogicHandler::LogicHandler(void){
+	titleScreen = true;
+
 	window = new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), TITLE, sf::Style::Close);
 
 	shape.setSize(sf::Vector2f(10.0f, 10.0f));
@@ -19,9 +21,11 @@ LogicHandler::LogicHandler(void){
 		shape.setFillColor(sf::Color::Green);
 	}
 
-	sprite.setTexture(*textureList.at(1));
-	//sprite.setTextureRect(sf::IntRect(10, 10, 50, 30));
-	sprite.setPosition(0, 0);
+	titleScreenBg.setTexture(*textureList.at(1));
+	titleScreenBg.setPosition(0, 0);
+
+	cloud1.setTexture(*textureList.at(2));
+	cloud2.setTexture(*textureList.at(2));
 }
 
 //DESTRUCTOR
@@ -31,7 +35,7 @@ LogicHandler::~LogicHandler(void){
 
 void LogicHandler::run(void){
 
-	double deltaTime = 0;
+	float deltaTime = 0;
 	sf::Clock clock;
 	while (window->isOpen()){
 		sf::Event event;
@@ -42,11 +46,18 @@ void LogicHandler::run(void){
 		clock.restart();
 		LogicHandler::update(deltaTime);
 
-		window->clear(sf::Color(255, 0, 255));
+		window->clear(sf::Color(20, 180, 255));
 
 		//render
-		window->draw(sprite);
+		if(titleScreen){
+			window->draw(titleScreenBg);
+		}else{
+			window->draw(cloud1);
+			window->draw(cloud2);
+		}
+
 		window->draw(shape);
+		shape.move(1, 0);
 
 		deltaTime = clock.getElapsedTime().asMilliseconds();
 
@@ -54,13 +65,20 @@ void LogicHandler::run(void){
     }
 }
 
-void LogicHandler::update(double delta){
-
+void LogicHandler::update(float delta){
+	if(!titleScreen){
+		cloud1.move(-0.01f * delta, 0.0f);
+		cloud2.setPosition(WIDTH + cloud1.getPosition().x, 0);
+	}
 }
 
 void LogicHandler::handleEvent(sf::Event evt){
-	if (evt.type == sf::Event::Closed)
+	if (evt.type == sf::Event::Closed){
 		window->close();
+	}else if((evt.type == sf::Event::KeyPressed ||
+		evt.type == sf::Event::MouseButtonPressed) && titleScreen){
+			titleScreen = false;
+	}
 }
 
 bool LogicHandler::loadTextures(std::string dir){
--- tifa/tifa/LogicHandler.h
+++ tifa/tifa/LogicHandler.h
@@ -9,12 +9,14 @@ class LogicHandler{
 public:
 	LogicHandler(void);	//CONSTRUCTOR
 	~LogicHandler(void);	//DESTRUCTOR
-	void update(double delta_time);	//update game stuff
+	void update(float delta_time);	//update game stuff
 	void run(void);	//main game loop
 
 private:
 	void handleEvent(sf::Event theEventToBeHandled);	//handles any and all events
 	bool loadTextures(std::string directory);
 	sf::RenderWindow *window;	//the main window (probobaly the only)
-	std::vector<sf::Texture*> textureList;
+	std::vector<sf::Texture*> textureList;	//list of all textures currently loaded
+	std::vector<sf::Sprite*> spriteList;	//list of all active sprites
+	bool titleScreen;	//if the game is at the title screen
 };
